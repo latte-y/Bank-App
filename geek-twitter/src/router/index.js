bfd52d4f2bd5421bb81e074b2d6c91a2143a31e3
latte-y <yukimkyn1119@gmail.com>
2022-05-15 14:33:00 +0900

    vue-routerを導入

--- geek-twitter/src/router/index.js
+++ geek-twitter/src/router/index.js
@@ -1,20 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
+import AboutView from "../views/AboutView.vue"
+import MyPageView from "../views/MyPageView.vue"
 
 const routes = [
   {
     path: "/",
-    name: "home",
+    name: "Home",
     component: HomeView,
   },
   {
     path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    name: "About",
+    component: AboutView,
+  },
+  {
+    path: "/my-page",
+    name: "MyPage",
+    component: MyPageView,
   },
 ]
 
